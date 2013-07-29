455dbfa120797f684a2ab66f3be58ad030ba38d2
mntone <sd8@live.jp>
2013-07-29 11:06:36 +0900

    First release (1.0.0.0).

--- Mntone.PerMonitorDpi/Properties/AssemblyInfo.cs
+++ Mntone.PerMonitorDpi/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Mntone.PerMonitorDpi")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2013 mntone and GitHub users")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
