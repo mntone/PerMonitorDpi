c957cd87275d4c69e67ae8df78a981b3f18934f0
mntone <sd8@live.jp>
2014-03-09 08:14:55 +0900

    Commit forgotten files.

---
+++ Mntone.PerMonitorDpiTestApplication/Views/Infrastructure/Win32/MonitorDefaultTo.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Mntone.PerMonitorDpiTestApplication.Views.Infrastructure.Win32
+{
+	public enum MonitorDefaultTo: int
+	{
+		Null = 0,
+		Primary = 1,
+		Nearest = 2,
+	}
+}
---
+++ Mntone.PerMonitorDpiTestApplication/Views/Infrastructure/Win32/NativeHelper.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Mntone.PerMonitorDpiTestApplication.Views.Infrastructure.Win32
+{
+	public static class NativeHelper
+	{
+		public static ushort GetLoWord( uint dword )
+		{
+			return ( ushort )( dword & 0xffff );
+		}
+
+		public static ushort GetHiWord( uint dword )
+		{
+			return ( ushort )( dword >> 16 );
+		}
+	}
+}
